6789b741b46f0037df0772b88e8085adb2806da1
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-04-09 15:50:10 +0500

    add: logics

--- src/components/SelectButtons.jsx
+++ src/components/SelectButtons.jsx
@@ -1,27 +1,41 @@
+import { useState } from "react";
+
 export default function SelectButtons() {
+  const arrNum0 = [
+    "/public/images/1.png",
+    "/public/images/2.png",
+    "/public/images/3.png",
+  ];
+  const arrNum1 = [
+    "/public/images/4.png",
+    "/public/images/5.png",
+    "/public/images/6.png",
+  ];
+  const [selectedNumber, setSelectedNumber] = useState();
+  console.log(selectedNumber);
   return (
     <>
-      <div className="game-btns select-btns">
-        <button className="select-btn">
-          <img src="/public/images/1.png" alt="" />
-        </button>
-        <button className="select-btn">
-          <img src="/public/images/2.png" alt="" />
-        </button>
-        <button className="select-btn">
-          <img src="/public/images/3.png" alt="" />
-        </button>
+      <div className="game-btns">
+        {arrNum0.map((value, i) => (
+          <button
+            onClick={() => setSelectedNumber(value)}
+            className="select-btn"
+            key={i}
+          >
+            <img src={value} alt="" />
+          </button>
+        ))}
       </div>
-      <div className="game-btns select-btns">
-        <button className="select-btn">
-          <img src="/public/images/4.png" alt="" />
-        </button>
-        <button className="select-btn">
-          <img src="/public/images/5.png" alt="" />
-        </button>
-        <button className="select-btn">
-          <img src="/public/images/6.png" alt="" />
-        </button>
+      <div className="game-btns">
+        {arrNum1.map((value, i) => (
+          <button
+            onClick={() => setSelectedNumber(value)}
+            className="select-btn"
+            key={i}
+          >
+            <img src={value} alt="" />
+          </button>
+        ))}
       </div>
     </>
   );
