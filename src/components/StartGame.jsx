169ed1c29d6c99f7f07824c074ca1ad8dda6a543
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-04-13 10:59:46 +0500

    add: path updated

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -4,7 +4,7 @@ export default function StartGame({ toggle }) {
       <div className="score">
         <h3>Start Game</h3>
         <div>
-          <img src="/images/start.jpg" alt="" />
+          <img src="/public/images/start.jpg" alt="" />
         </div>
       </div>
       <div className="game-btns">
